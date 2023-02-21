62d16a2b829b1daa875a2ecf7ef1def3bd01a038
Michael Thomas <michaelhthomas@outlook.com>
2023-02-21 15:01:29 -0500

    update JSONParser singleTriangleTest

--- test/input/parser/JSONParserTest.java
+++ test/input/parser/JSONParserTest.java
@@ -2,10 +2,13 @@ package input.parser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import input.components.ComponentNode;
 import input.components.FigureNode;
+import input.components.point.PointNode;
 import input.exception.ParseException;
 
 /**
@@ -36,13 +39,25 @@ class JSONParserTest {
 
 	@Test
 	void singleTriangleTest() {
-
 		ComponentNode node = JSONParserTest.runFigureParseTest("testFiles/single_triangle.json");
 
 		assertTrue(node instanceof FigureNode);
+		FigureNode figNode = (FigureNode) node;
 
 		StringBuilder sb = new StringBuilder();
 		node.unparse(sb, 0);
+
+		// TODO: ask Dr. Alvin abt this
 		System.out.println(sb.toString());
+
+		List<PointNode> trianglePoints = List.of(
+				new PointNode(0, 0),
+				new PointNode(1, 1),
+				new PointNode(1, 0));
+
+		assertEquals("Right Triangle in the first quadrant.", figNode.getDescription());
+		for (PointNode p : trianglePoints)
+			assertTrue(figNode.getPointsDatabase().contains(p));
+		assertEquals(3, figNode.getSegments().asUniqueSegmentList().size());
 	}
 }
