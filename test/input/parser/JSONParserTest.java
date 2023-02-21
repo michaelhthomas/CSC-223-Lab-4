feeae0aada148c863f09b0159c8d3f74840bf074
Michael Thomas <michaelhthomas@outlook.com>
2023-02-21 15:45:20 -0500

    add JSONParser compareJsonToUnparseTest

--- test/input/parser/JSONParserTest.java
+++ test/input/parser/JSONParserTest.java
@@ -2,9 +2,14 @@ package input.parser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import input.components.ComponentNode;
 import input.components.FigureNode;
@@ -20,12 +25,27 @@ import input.exception.ParseException;
  */
 
 class JSONParserTest {
-	public static ComponentNode runFigureParseTest(String filename) {
-		JSONParser parser = new JSONParser();
-
+	public static String getFigureJSON(String filename) {
 		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filename);
 
-		return parser.parse(figureStr);
+		return figureStr;
+	}
+
+	public static ComponentNode getFigureFromJson(String json) {
+		JSONParser parser = new JSONParser();
+		return parser.parse(json);
+	}
+
+	public static ComponentNode runFigureParseTest(String filename) {
+		String figureStr = getFigureJSON(filename);
+		return getFigureFromJson(figureStr);
+	}
+
+	public static Stream<String> provideStringsForCompareJsonToUnparse() {
+		File f = new File("testFiles/");
+		return Arrays.stream(f.list()).map(s -> {
+			return "testFiles/" + s;
+		});
 	}
 
 	@Test
@@ -37,18 +57,31 @@ class JSONParserTest {
 		});
 	}
 
+	@ParameterizedTest
+	@MethodSource("provideStringsForCompareJsonToUnparse")
+	void compareJsonToUnparseTest(String filename) {
+		System.out.println(filename);
+
+		String nodeJson = getFigureJSON(filename);
+
+		System.out.println("JSON representation: \n" + nodeJson);
+
+		ComponentNode node = getFigureFromJson(nodeJson);
+
+		assertTrue(node instanceof FigureNode);
+
+		StringBuilder sb = new StringBuilder();
+		node.unparse(sb, 0);
+
+		System.out.println("Unwrapped: \n" + sb.toString());
+	}
+
 	@Test
 	void singleTriangleTest() {
 		ComponentNode node = JSONParserTest.runFigureParseTest("testFiles/single_triangle.json");
 
 		assertTrue(node instanceof FigureNode);
 		FigureNode figNode = (FigureNode) node;
-
-		StringBuilder sb = new StringBuilder();
-		node.unparse(sb, 0);
-
-		// TODO: ask Dr. Alvin abt this
-		System.out.println(sb.toString());
 
 		List<PointNode> trianglePoints = List.of(
 				new PointNode(0, 0),
